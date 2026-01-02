7435f9c3ec9554b0bf6ed805e56facf7f9452c04
kalyan7104 <kalyankumarmunjuluri@gmail.com>
2026-01-02 09:45:18 +0530

    changes for production

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  const port=process.env.PORT || 3000;
+  await app.listen(port);
 }
 void bootstrap();
